7128ca08111edd013ebc734dd97fdb957154cef5
pmNiko <nikolas090189@gmail.com>
2020-10-18 15:49:37 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {useState ,useEffect} from 'react';
+const Mouse = () => {
+  const [mouse, setMouse] = useState([0,0])
+
+  useEffect( () => {
+    console.log('inicio');
+  })
+}
+
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
